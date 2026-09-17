e081811a34c4c9e10ed8e9d00d7b46d1cfb8bf0a
Quin Nordmark <quinnordmark@gmail.com>
2026-09-17 16:35:12 -0600

    Point API base at Fly.io backend

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://newspaper-backend-w3kl.onrender.com/api";
+const API_BASE = "https://paper-of-love-backend.fly.dev/api";
 const API_ORIGIN = API_BASE.replace(/\/api$/, "");
 
 async function apiRequest(path, options) {
